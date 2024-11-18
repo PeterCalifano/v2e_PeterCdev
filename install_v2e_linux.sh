cbf0e2af12dfd54b45293a8a8990dc6726af6a9c
PeterC <petercalifano.gs@gmail.com>
2024-11-18 21:33:20 +0100

    Updated gitignore

--- install_v2e_linux.sh
+++ install_v2e_linux.sh
@@ -2,4 +2,4 @@
 #source .venvEventBased/bin/activate
 python3.11 -m pip install -e . --require-virtualenv
 pip install torch torchvision --require-virtualenv
-pip install dv-processing --require-virtualenv
+pip install dv-processing pillow==9.5.0 --require-virtualenv
