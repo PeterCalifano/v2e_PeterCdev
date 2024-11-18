dd618f80d09e199b9956fab66e166dd52e3858d9
PeterC <petercalifano.gs@gmail.com>
2024-11-18 23:10:07 +0100

    Added script for quick use of v2e

---
+++ frames2events_quick.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Check if exactly two arguments are passed
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <input1> <input2>"
+    exit 1
+fi
+
+# Assign inputs to variables
+input_folder=$1
+input_framerate=$2
+
+echo "Input folder path: $input_folder"
+echo "Input framerate: $input_framerate"
+
+python v2e.py -i $input_folder --input_frame_rate $input_framerate --auto_timestamp_resolution --dvs640 --dvs_params 'clean'
