5195b7a83d3191a18a77976cd1955b8745534f8f
PeterC <petercalifano.gs@gmail.com>
2024-12-07 11:40:10 +0100

    Added symlink generation (requires sudo)

--- install_v2e_linux.sh
+++ install_v2e_linux.sh
@@ -3,3 +3,7 @@ source .venvEventBased/bin/activate
 python3.11 -m pip install -e . --require-virtualenv
 pip install torch torchvision --require-virtualenv
 pip install dv-processing pillow==9.5.0 --require-virtualenv
+
+# Add symlink to v2e.py in system location
+sudo ln -s $HOME/devDir/event-based-repos/v2e_PeterCdev/v2e.py /usr/local/bin/v2e
+sudo chmod +xr v2e
