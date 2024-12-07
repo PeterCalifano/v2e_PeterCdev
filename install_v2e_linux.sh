be35071a5344e52c49768a70522787538ef3d425
PeterC <petercalifano.gs@gmail.com>
2024-12-07 11:35:05 +0100

    MInor changes

--- install_v2e_linux.sh
+++ install_v2e_linux.sh
@@ -1,5 +1,5 @@
-#python3.11 -m venv .venvEventBased
-#source .venvEventBased/bin/activate
+python3.11 -m venv .venvEventBased
+source .venvEventBased/bin/activate
 python3.11 -m pip install -e . --require-virtualenv
 pip install torch torchvision --require-virtualenv
 pip install dv-processing pillow==9.5.0 --require-virtualenv
