842d67aade5a174ebc0411f1036ef5c6d16c73c1
PeterC <petercalifano.gs@gmail.com>
2024-12-07 12:05:56 +0100

    Added script code to install v2e at system level using symlinks (experimental)

--- frames2events_quick.sh
+++ frames2events_quick.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+# Source the virtual environment in the root folder of the v2e repository
+DIR="$( cd "$( dirname "$(readlink -f "${BASH_SOURCE[0]}")" )" && pwd )"
+
+echo "frame2events_quick script directory: $DIR"
+
+v2e_location=$(which v2e)
+echo "v2e location: $v2e_location"
+
+# If empty string, then v2e is not in the PATH
+if [ -z "$v2e_location" ]; then
+    echo "v2e is not in the PATH. Have you installed v2e by running install_v2e_linux? There should be a symlink in /usr/local/bin."
+    exit 1
+fi
+
+# Save the current directory
+current_dir=$(pwd)
+
+cd $DIR
+source .venvEventBased/bin/activate
+cd $current_dir
+
 # Check if exactly two arguments are passed
 if [ "$#" -ne 3 ]; then
     echo "Usage: $0 <input_folder> <input_framerate> <output_folder>"
@@ -14,4 +35,8 @@ output_folder=$3
 echo "Input folder path: $input_folder"
 echo "Input framerate: $input_framerate"
 
-python v2e.py -i $input_folder --input_frame_rate $input_framerate --auto_timestamp_resolution --dvs_params 'clean' --batch_size 8 --output-folder $output_folder --input_slowmotion_factor 1 --output_height 512 --output_width 512 --dvs_exposure duration 0.1
+# Get location of v2e.py from whichs
+python $v2e_location -i $input_folder --input_frame_rate $input_framerate --auto_timestamp_resolution --dvs_params 'clean' --batch_size 8 --output-folder $output_folder --input_slowmotion_factor 1 --output_height 512 --output_width 512 --dvs_exposure duration 0.1
+
+# Deactivate the virtual environment
+#deactivate
--- install_v2e_linux.sh
+++ install_v2e_linux.sh
@@ -7,3 +7,7 @@ pip install dv-processing pillow==9.5.0 --require-virtualenv
 # Add symlink to v2e.py in system location
 sudo ln -s $HOME/devDir/event-based-repos/v2e_PeterCdev/v2e.py /usr/local/bin/v2e
 sudo chmod +xr v2e
+
+# Add symlink to frames2events_quick.sh in system location
+sudo ln -s $HOME/devDir/event-based-repos/v2e_PeterCdev/frames2events_quick.sh /usr/local/bin/frames2events_quick
+sudo chmod +xr frames2events_quick
