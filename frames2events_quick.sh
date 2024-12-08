90045350eb31384f450640546a4434976582afb3
PeterC <petercalifano.gs@gmail.com>
2024-12-08 12:36:41 +0100

    Updated script with new args

--- frames2events_quick.sh
+++ frames2events_quick.sh
@@ -22,21 +22,30 @@ source .venvEventBased/bin/activate
 cd $current_dir
 
 # Check if exactly two arguments are passed
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <input_folder> <input_framerate> <output_folder>"
+if [ "$#" -lt 2 ]; then
+    echo "Usage: $0 <input_folder> <input_framerate>"
     exit 1
 fi
+
+# Check if the third argument is passed
+if [ "$#" -lt 3 ]; then
+    echo "Event threshold not provided. Using default value of 0.005."
+fi
+event_thr=${3:-0.07}  # Set a default value if the third argument is not provided
 
 # Assign inputs to variables
 input_folder=$1
 input_framerate=$2
-output_folder=$3
+#output_folder=$3
 
 echo "Input folder path: $input_folder"
 echo "Input framerate: $input_framerate"
+echo "Event threshold: $event_thr"
 
 # Get location of v2e.py from whichs
-python $v2e_location -i $input_folder --input_frame_rate $input_framerate --auto_timestamp_resolution --dvs_params 'clean' --batch_size 8 --output-folder $output_folder --input_slowmotion_factor 1 --output_height 512 --output_width 512 --dvs_exposure duration 0.1
+#--auto_timestamp_resolution
+#--timestamp_resolution 0.05
+python $v2e_location -i $input_folder --input_frame_rate $input_framerate --pos_thres $event_thr --neg_thres $event_thr --timestamp_resolution 0.0001 --auto_timestamp_resolution --batch_size 32 --output_height 256 --output_width 256 --slomo_stats_plot 
 
 # Deactivate the virtual environment
 #deactivate
